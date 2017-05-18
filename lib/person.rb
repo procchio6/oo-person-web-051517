b90746ef8203f17acbbdb594e4565cfa46a0ece3
Pat Rocchio <patrocc6@gmail.com>
2017-05-17 21:24:54 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,83 @@
-# your code goes here
+require "pry"
+
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(happiness_value)
+    @happiness = set_value.call(happiness_value)
+  end
+
+  def hygiene=(hygiene_value)
+    @hygiene = set_value.call(hygiene_value)
+  end
+
+  def happy?
+    @happiness > 7 ? true : false
+  end
+
+  def clean?
+    @hygiene > 7 ? true : false
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(person)
+    self.happiness += 3
+    person.happiness += 3
+    "Hi #{person.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    case topic
+    when "politics"
+      self.happiness -= 2
+      person.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      self.happiness += 1
+      person.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+
+  private
+
+  def set_value
+    Proc.new do |value|
+      result = nil
+      if value > 10
+         result = 10
+      elsif value < 0
+        result = 0
+      else
+        result = value
+      end
+      result
+    end
+  end
+
+end
